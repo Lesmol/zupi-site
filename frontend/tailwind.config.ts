97a909bbfdff11d2ad5edfc3b96d2d1e6b9eeb07
Lesedi <molemilesedi@gmail.com>
2024-04-18 21:41:15 +0200

    added responsive hero section

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,20 +1,35 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+        "2xl": "1536px",
+      },
+      colors: {
+        primary: "#583900",
+        "lighter-primary": "#C98200",
+        red: "#FF0000",
+        "darker-red": "#D2042D",
+      },
+      backgroundColor: {
+        orange: "#FFA500",
+        "light-orange": "#FFD78D",
+      },
+      aspectRatio: {
+        "752/444": "752/444",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
