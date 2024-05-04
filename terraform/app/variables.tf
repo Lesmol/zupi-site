34aa2b961e873c05da9e54312f4cbafc292f0749
Lesedi <molemilesedi@gmail.com>
2024-05-05 00:01:43 +0200

    -Added first workflow

---
+++ terraform/app/variables.tf
@@ -0,0 +1,29 @@
+variable "location" {
+  type        = string
+  description = "The Azure Region in which all resources groups should be created."
+}
+
+variable "rg-name" {
+  type        = string
+  description = "The name of the realtime chat resource group"
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "The subscription id of the realtime chat service principal"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "The tenant id of the realtime chat service principal"
+}
+
+variable "client_id" {
+  type        = string
+  description = "The client id of the realtime chat service principal"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "The client secret of the realtime chat service principal"
+}
