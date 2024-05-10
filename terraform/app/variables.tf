f5c5f2ac7a21b36d1ebb475fae2b34825c06a41f
Lesedi <molemilesedi@gmail.com>
2024-05-10 22:13:58 +0200

    Updated terraform-plan-apply.yml

--- terraform/app/variables.tf
+++ terraform/app/variables.tf
@@ -8,17 +8,17 @@ variable "rg-name" {
   description = "The name of the realtime chat resource group"
 }
 
-variable "subscription_id" {
-  type        = string
-  description = "The subscription id of the realtime chat service principal"
-}
+# variable "subscription_id" {
+#   type        = string
+#   description = "The subscription id of the realtime chat service principal"
+# }
 
-variable "tenant_id" {
-  type        = string
-  description = "The tenant id of the realtime chat service principal"
-}
+# variable "tenant_id" {
+#   type        = string
+#   description = "The tenant id of the realtime chat service principal"
+# }
 
-variable "client_id" {
-  type        = string
-  description = "The client id of the realtime chat service principal"
-}
+# variable "client_id" {
+#   type        = string
+#   description = "The client id of the realtime chat service principal"
+# }
