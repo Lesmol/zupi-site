98d10b30fca6d905c0bce3af06543c12ab10853c
Lesedi <molemilesedi@gmail.com>
2024-05-10 09:43:14 +0200

    Added terraform configuration variables to provider

--- terraform/app/providers.tf
+++ terraform/app/providers.tf
@@ -7,10 +7,13 @@ terraform {
   }
   # Remote backend
   backend "azurerm" {
-  use_oidc = true
   }
 }
 
 provider "azurerm" {
+  use_oidc = true
   features {}
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  tenant_id = var.tenant_id
 }
