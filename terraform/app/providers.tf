4910d1062c35d6dad1c4facaadc7dee5f83daac9
Lesedi Molemi <molemilesedi@gmail.com>
2024-05-11 11:07:08 +0200

    Infrastructure (#2)
    * -Added first workflow
    
    * Modified env variable names, terraform init and terraform plan
    
    * Removing pull_request rules to show github actions in the tab
    
    * Brought back pull request rules
    
    * Added trigger for on push to infrastucture branch
    
    * Removed on push to infrastucture branch trigger
    
    * Added client secret to terraform init backend-config
    
    * Updated client secret backend config
    
    * Authenticate with oidc set to true
    
    * Updated backend in providers.tf
    
    * Added terraform configuration variables to provider
    
    * Added client_secret to pipeline and provider
    
    * Updated providers.tf
    
    * Commented out terraform init
    
    * Added on push to infrastucture
    
    * Uncommented terraform init
    
    * Removed -backend-config from terraform plan
    
    * Added TF_VAR
    
    * Remove TF_VAR
    
    * Added variables to terraform plan
    
    * Updated terraform init
    
    * Updated env
    
    * Added ARM variables
    
    * Formatted yaml file
    
    * Removed client secret variable
    
    * Updated terraform-plan-apply.yml
    
    * Removed resource_group from backend
    
    * Added resource group to workflow
    
    * Added use azure ad auth
    
    * Updated yml file and provider.tf
    
    * Added -backend-cofig for resource_group_name
    
    * Added -backend-config resource_group_name to Terraform apply job
    
    * Using ARM in Terraform apply
    
    * Reverted
    
    * Added -backend-config resource_group_name to Terraform apply
    
    * Removed on push trigger from terraform-plan-apply.yml
    
    * - Added terraform-destroy.yml
    -Added terraform-plan.yml
    - Updated workflows to have workflow-wide env variables and use less jobs
    
    * Added permisions to id-tokens in the workflow
    
    * Updated terraform-plan
    
    * Updated terraform-plan.yml
    
    * Changed rg-name variable name to rg_name
    
    * Changed rg-name variable name to rg_name in terraform-destroy.yml & terraform-plan-apply.yml

---
+++ terraform/app/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.93.0"
+    }
+  }
+
+  # Remote backend
+  backend "azurerm" {
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
---
+++ terraform/app/variables.tf
@@ -0,0 +1,24 @@
+variable "location" {
+  type        = string
+  description = "The Azure Region in which all resources groups should be created."
+}
+
+variable "rg-name" {
+  type        = string
+  description = "The name of the realtime chat resource group"
+}
+
+# variable "subscription_id" {
+#   type        = string
+#   description = "The subscription id of the realtime chat service principal"
+# }
+
+# variable "tenant_id" {
+#   type        = string
+#   description = "The tenant id of the realtime chat service principal"
+# }
+
+# variable "client_id" {
+#   type        = string
+#   description = "The client id of the realtime chat service principal"
+# }
