3ef91a61216ec89067ca39ab883b3afbe2cfd191
Lesedi <molemilesedi@gmail.com>
2024-07-06 17:03:17 +0200

    Updated .gitignore file

--- api/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+644bd00cf0684ae5a89036f13ca2b1848f413bbb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2a5e6482864ccf6dd5eb78c8b2377460243b1a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
