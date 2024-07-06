f2a5e6482864ccf6dd5eb78c8b2377460243b1a6
Lesedi <molemilesedi@gmail.com>
2024-07-06 16:53:35 +0200

    Update .gitignore to exclude bin and obj directories

--- api/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4330f7e85a487e558d3f1666eccfd18f29a8b918")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+644bd00cf0684ae5a89036f13ca2b1848f413bbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
