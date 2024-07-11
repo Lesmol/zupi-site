5c8a55d16216b4efa9ffc320f257ce95ebfc5c17
Lesedi <molemilesedi@gmail.com>
2024-07-11 20:39:24 +0200

    commented out storage-account-name

--- terraform/app/variables.tf
+++ terraform/app/variables.tf
@@ -1,29 +1,11 @@
-variable "location" {
-  type        = string
-  description = "The Azure Region in which all resources groups should be created."
-}
+variable "location" {}
 
-variable "rg-name" {
-  type        = string
-  description = "The name of the realtime chat resource group"
-}
+variable "rg-name" {}
 
-variable "storage-account-name" {
-  type        = string
-  description = "The name of the realtime chat resource group"
-}
+# variable "storage-account-name" {}
 
-# variable "subscription_id" {
-#   type        = string
-#   description = "The subscription id of the realtime chat service principal"
-# }
+# variable "subscription_id" {}
 
-# variable "tenant_id" {
-#   type        = string
-#   description = "The tenant id of the realtime chat service principal"
-# }
+# variable "tenant_id" {}
 
-# variable "client_id" {
-#   type        = string
-#   description = "The client id of the realtime chat service principal"
-# }
+# variable "client_id" {}
