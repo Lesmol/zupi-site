8e2c1d464a996a4f8b9482d7aabc9480e6f4c723
Lesedi <molemilesedi@gmail.com>
2024-07-15 10:30:49 +0200

    set always on to false in site config

--- terraform/app/main.tf
+++ terraform/app/main.tf
@@ -25,5 +25,7 @@ resource "azurerm_linux_web_app" "zupi_web_app" {
   service_plan_id     = azurerm_service_plan.zupi_service_plan.id
   tags                = azurerm_resource_group.zupi_rg.tags
 
-  site_config {}
+  site_config {
+    always_on = false
+  }
 }
